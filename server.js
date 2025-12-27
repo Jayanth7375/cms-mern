1469139c0638e2203cbfcf0bd74376fe13a79c51
Jayanth7375 <ejayanth2006@gmail.com>
2025-12-27 10:26:58 +0530

    Fix auth login logic and remove CMS dependency

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ import authRoutes from "./routes/auth.routes.js";
 import departmentRoutes from "./routes/department.routes.js";
 import courseRoutes from "./routes/course.routes.js";
 import staffRoutes from "./routes/staff.routes.js";
-import cmsRoutes from "./routes/cms.routes.js";
 import adminRoutes from "./routes/admin.routes.js";
 
 
@@ -24,7 +23,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/departments", departmentRoutes);
 app.use("/api/courses", courseRoutes);
 app.use("/api/staffs", staffRoutes);
-app.use("/api/pages", cmsRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
 
